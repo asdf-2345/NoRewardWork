571c4946284bf829b65f658b70ada9dd827da947
asdf-2345 <kiaaa741@gmail.com>
2020-02-09 12:59:23 +0900

    업로드

---
+++ NoRewardWork/Assets/a.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class a : MonoBehaviour
+{
+	public int score;
+	public Text scoreText;
+	
+	void Update()
+    {
+		scoreText.text = "현재점수 : " + score.ToString();
+    }
+	
+	public void scoreUp(){
+		score++;
+	}
+}
